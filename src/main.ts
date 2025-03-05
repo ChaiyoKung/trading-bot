2576ae8870a1d1b42e0009e949713aa4de3688bc
ChaiyoKung <kananek45@gmail.com>
2025-03-05 23:30:26 +0700

    feat: implement trading logic for buy/sell market orders based on EMA trends

--- src/main.ts
+++ src/main.ts
@@ -46,3 +46,34 @@ console.table([
   { period: 100, prev: prevEma100, latest: latestEma100 },
   { period: 200, prev: prevEma200, latest: latestEma200 },
 ]);
+
+const isLatestUpTrend = latestEma20 > latestEma50 && latestEma50 > latestEma100 && latestEma100 > latestEma200;
+const isPrevUpTrend = prevEma20 > prevEma50 && prevEma50 > prevEma100 && prevEma100 > prevEma200;
+
+const isLatestDownTrend = latestEma20 < latestEma50 && latestEma50 < latestEma100 && latestEma100 < latestEma200;
+const isPrevDownTrend = prevEma20 < prevEma50 && prevEma50 < prevEma100 && prevEma100 < prevEma200;
+
+// create buy market order if prev is not up trend and latest is up trend and latest close price is higher than latest ema20
+if (!isPrevUpTrend && isLatestUpTrend && latestClosePrice > latestEma20) {
+  console.log("create buy market order");
+}
+
+// cancel buy market order if prev is up trend and latest is up trend and latest close price is lower than latest ema20
+else if (isPrevUpTrend && isLatestUpTrend && latestClosePrice < latestEma20) {
+  console.log("cancel buy market order");
+}
+
+// create sell market order if prev is not down trend and latest is down trend and latest close price is lower than latest ema20
+else if (!isPrevDownTrend && isLatestDownTrend && latestClosePrice < latestEma20) {
+  console.log("create sell market order");
+}
+
+// cancel sell market order if prev is down trend and latest is down trend and latest close price is higher than latest ema20
+else if (isPrevDownTrend && isLatestDownTrend && latestClosePrice > latestEma20) {
+  console.log("cancel sell market order");
+}
+
+// do nothing
+else {
+  console.log("do nothing");
+}
