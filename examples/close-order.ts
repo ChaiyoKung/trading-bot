eccbe79bbc5e08a27b4844bd242822146e4d2655
ChaiyoKung <kananek45@gmail.com>
2025-03-05 22:41:05 +0700

    init: v2 from clean

--- examples/close-order.ts
+++
@@ -1,5 +0,0 @@
-import { BingX } from '../src/exchanges';
-
-const exchange = new BingX('BTC-USDT', '5m', 125);
-await exchange.closeAllPositions();
-await exchange.cancelAllOrders();
--- examples/create-order.ts
+++
@@ -1,6 +0,0 @@
-import { BingX } from '../src/exchanges';
-
-const exchange = new BingX('BTC-USDT', '5m', 125);
-const ticker = await exchange.fetchTicker();
-if (ticker.last === undefined) throw new Error('`ticker.last` is undefined');
-await exchange.createMarketSellOrder(1, ticker.last);
