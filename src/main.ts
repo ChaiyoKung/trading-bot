4e02153067ad31dfa544bfec5d7d4099d3a81f85
ChaiyoKung <kananek45@gmail.com>
2025-04-02 00:04:04 +0700

    feat: enable sandbox mode for market fetching in non-production environment

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 import { FourEmaTrend } from "./strategies/four-ema-trend";
 import { BingX } from "./exchanges/bingx";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const exchange = new BingX();
-exchange.setSandboxMode(!isProd);
 
-console.log("Fetching markets...");
-const markets = await exchange.fetchMarkets();
-console.table(
-  markets
-    .filter((market) => market !== undefined)
-    .map((market) => market.symbol)
-    .filter((symbol) => symbol.includes("BTC"))
-);
+const isProd = process.env.NODE_ENV === "production";
+if (!isProd) {
+  console.log("Running in sandbox mode...");
+  exchange.setSandboxMode(true);
+
+  console.log("Fetching markets...");
+  const markets = await exchange.fetchMarkets();
+  console.table(
+    markets
+      .filter((market) => market !== undefined)
+      .map((market) => market.symbol)
+      .filter((symbol) => symbol.includes("BTC"))
+  );
+}
 
 const symbol = "BTC/USDT:USDT";
 const timeframe = "4h";
